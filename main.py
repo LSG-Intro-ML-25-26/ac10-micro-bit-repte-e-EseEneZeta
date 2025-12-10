d4337aae26d2c90b78f02c8ad41e0732718c8bc2
Mateo Achá <157355023+EseEneZeta@users.noreply.github.com>
2025-12-10 23:31:41 +0100

    accelerometre terminado

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+x = 2
+y = 2
 
+while True:
+    led.plot(x, y)
+    basic.pause(100)
+    led.unplot(x, y)
+    acc_x = input.acceleration(Dimension.X)
+    acc_y = input.acceleration(Dimension.Y)
+    if acc_x < -150 and x > 0:
+        x = x - 1
+    elif acc_x > 150 and x < 4:
+        x = x + 1
+    if acc_y < -150 and y > 0:
+        y = y - 1
+    elif acc_y > 150 and y < 4:
+        y = y + 1
--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+let acc_x: number;
+let acc_y: number;
+let x = 2
+let y = 2
+while (true) {
+    led.plot(x, y)
+    basic.pause(100)
+    led.unplot(x, y)
+    acc_x = input.acceleration(Dimension.X)
+    acc_y = input.acceleration(Dimension.Y)
+    if (acc_x < -150 && x > 0) {
+        x = x - 1
+    } else if (acc_x > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (acc_y < -150 && y > 0) {
+        y = y - 1
+    } else if (acc_y > 150 && y < 4) {
+        y = y + 1
+    }
+    
+}
